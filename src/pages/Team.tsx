852c318455ec17f35ff10efe8982ca69d7c6b636
Data Polaris <147243877+clubdatapolaris@users.noreply.github.com>
2026-02-23 23:42:14 +0530

    Update Team.tsx

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -6,7 +6,7 @@ import Card from "../components/ui/card";
 
 const Team: React.FC = () => {
   const coreTeam = [
-    {
+   {
       name: "Sudarshan Bhosale",
       role: "Head",
       branch:"",
@@ -19,12 +19,10 @@ const Team: React.FC = () => {
       name: "Sayali Jadhav",
       role: "Co-Head",
       branch:"",
-      image: "/images/team images/Sayali Jadhav.jpg",
+      image: "/images/team images/Sayali Jadhav.jpeg",
       github: "https://github.com",
       linkedin: "https://www.linkedin.com/in/sayali-jadhav-b4263827b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      imageStyle: {objectfit: 'cover',
-        objectPosition: 'center -2px',
-        }  
+      
     },
   ];
 
@@ -33,15 +31,15 @@ const Team: React.FC = () => {
     {
       name: "Nilesh Patil",
       role: "",
-      image: "/images/team images/Nilesh Patil.jpg",
+      image: "/images/team images/Nilesh Patil.jpeg",
       github: "https://github.com/NileshPatil24-a",
       linkedin: "www.linkedin.com/in/nilesh-patil-a3818b329",
-      imageStyle: { objectPosition: 'center 0px' } 
+      imageStyle: { objectPosition: 'center -10px' } 
     },
     {
       name: "Ashna Pinjari",
       role: "",
-      image: "/images/team images/Ashna Pinjari.jpg",
+      image: "/images/team images/Ashna Pinjari.jpeg",
       github: "https://github.com/ashhgit45",
       linkedin: "https://www.linkedin.com/in/ashna-pinjari-b067942a0",
    
@@ -49,19 +47,17 @@ const Team: React.FC = () => {
     {
       name: "Riya Deshmukh",
       role: "",
-      image: "/images/team images/Riya Deshmukh.jpg",
+      image: "/images/team images/Riya Deshmukh.jpeg",
       github: "https://github.com/Rhea-droid",
       linkedin: "https://www.linkedin.com/in/riya-deshmukh-557b182b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      imageStyle:{ objectPosition: 'center -50px'},
       
     },
     {
       name: "Paresh Mahirrao",
       role: "",
-      image: "/images/team images/Paresh Mahirrao.jpg",
+      image: "/images/team images/Paresh Mahirrao.jpeg",
       github: "https://github.com",
       linkedin: "https://www.linkedin.com/in/paresh-mahirrao-389825250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      imageStyle: { objectPosition: 'center -2px'},
        
     },
     {
@@ -75,18 +71,17 @@ const Team: React.FC = () => {
     {
       name: "Digvijaysing Rajput",
       role: "",
-      image: "/images/team images/Digvijaysing Rajput.jpg",
+      image: "/images/team images/DigvijaySingh Rajput.jpeg",
       github: "https://github.com/digvijay1129",
       linkedin: "https://www.linkedin.com/in/digvijaysing-rajput-287b46351?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-
-      imageStyle: {  objectPosition: 'center -55px'},
+     imageStyle: { objectPosition: 'center 0px' } 
         
     },
     
     {
       name: "Disha Nikumbh",
       role: "",
-      image: "/images/team images/Disha Nikum.jpg",
+      image: "/images/team images/Disha Nikum.jpeg",
       github: "https://github.com/dishanikumbh06",
       linkedin: "https://www.linkedin.com/in/disha-rajendra-nikumbh-9873bb290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
   
@@ -94,20 +89,19 @@ const Team: React.FC = () => {
     {
       name: "Pranav Sutar",
       role: "",
-      image: "/images/team images/Pranav Sutar.jpg",
+      image: "/images/team images/Pranav Sutar.jpeg",
       github: "https://github.com/pranav1217",
       linkedin: "https://www.linkedin.com/in/pranav-sutar-0358642b2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       
-      imageStyle: {    objectPosition: 'center -53px'},
-
     },
   
     {
       name: "Parag Nikum",
       role: "",
-      image: "/images/team images/Parag Nikum.jpg",
+      image: "/images/team images/Parag Nikum.jpeg",
       github: "https://github.com/Parag-Nikum",
       linkedin: "https://www.linkedin.com/in/parag-nikum-a371aa294?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      imageStyle: { objectPosition: 'center 0px' } 
     },
     {
       name: "Bhavya Chauhan",
@@ -122,152 +116,133 @@ const Team: React.FC = () => {
       image: "/images/team images/Paragi Patil.jpg",
       github: "https://github.com/paragi2005",
       linkedin: "https://www.linkedin.com/in/paragi-patil-aab83b2bb",
-       imageStyle: { objectPosition: 'center -55px'},
-      
+       imageStyle: { objectPosition: 'center 0px' } 
     },
     {
       name: "Pushpraj Patil",
       role: "",
-      image: "/images/team images/Pushpraj Patil.jpg",
+      image: "/images/team images/Pushpraj Patil.jpeg",
       github: "https://github.com/pushpraj135/pushpa",
       linkedin: "http://www.linkedin.com/in/pushpraj-patil-8631a528a",
-      imageStyle: {  objectPosition: 'center -50px'},
       
     },
      {
       name: "Vedant Badgujar",
       role: "",
-      image: "/images/team images/Vedant Badgujar.jpg",
+      image: "/images/team images/Vedant Badgujar.jpeg",
       github: "https://github.com/satorucommit",
       linkedin: "https://in.linkedin.com/in/vedant-badgujar-a546bb298",
-     imageStyle: {  objectFit: 'cover',
-                  objectPosition: 'center -55px' }  
+     
     },
     {
       name: "Amar Lajge",
       role: "",
-      image: "/images/team images/Amar Lajge.jpg",
+      image: "/images/team images/Amar Lajge.jpeg",
       github: "https://github.com/Alajge18",
       linkedin: "https://www.linkedin.com/in/amar-lajge-504ba432a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-   imageStyle: {  objectFit: 'cover',
-                  objectPosition: 'center -40px' }  
-    
+     imageStyle: { objectPosition: 'center 0px' } 
     },
     {
       name: "Aadesh Khairnar",
       role: "",
-      image: "/images/team images/Aadesh Khairnar.jpg",
+      image: "/images/team images/Aadesh Khairnar.jpeg",
       github: "https://github.com/khairnaraadesh1-droid",
       linkedin: "https://www.linkedin.com/in/aadesh-khairnar-1392a832a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      imageStyle: {   objectPosition: 'center 0px'},
-       
+       imageStyle: { objectPosition: 'center 0px' } 
     },
      {
       name: "Sakshi Shimpi",
       role: "",
-      image: "/images/team images/Sakshi Shimpi.jpg",
+      image: "/images/team images/Sakshi Shimpi.jpeg",
       github: "https://github.com/sakshishimpi11",
       linkedin: "https://www.linkedin.com/in/sakshi-shimpi-422318338/",
-      imageStyle: {   objectPosition: 'center -55px'},
-       
+
     },
     {
       name: "Devansh Chaudhari",
       role: "",
-      image: "/images/team images/Devansh Chaudhari.jpg",
+      image: "/images/team images/Devansh Chaudhari.jpeg",
       github: "https://github.com/devansh13-tech",
-      linkedin: "https://www.linkedin.com/in/devansh-chaudhari-33a360247",
-      imageStyle: {    objectPosition: 'center -45px'},
-    
+      linkedin: "https://www.linkedin.com/in/devansh-chaudhari-33a360247",    
     },
      {
       name: "Pranav Chaudhari",
       role: "",
-      image: "/images/team images/Pranav Chaudhari.jpg",
+      image: "/images/team images/Pranav Chaudhari.jpeg",
       github: "https://github.com/Pranav-Chaudhari07",
       linkedin: "https://www.linkedin.com/in/pranav-chaudhari-78203a298=android_app",
-      imageStyle: {  objectPosition: 'center -45px'},
       
     },
     {
       name: "Amey Sangpal",
       role: "",
-      image: "/images/team images/Amey Sangpal.jpg",
+      image: "/images/team images/Amey Sangpal.jpeg",
       github: "https://github.com/ameysangpal110",
       linkedin: "https://www.linkedin.com/in/amey-sangpal-53b102325/",
-      imageStyle: {   objectPosition: 'center -53px'},
        
     },
     {
       name: "Tejas Zalte",
       role: "",
-      image: "/images/team images/Tejas Zalte.jpg",
+      image: "/images/team images/Tejas Zalte.jpeg",
       github: "https://github.com/TejasZalte",
       linkedin: "https://www.linkedin.com/in/tejas-zalte-9a7b8b32b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-     imageStyle: {  objectPosition: 'center -53px'},
       
     },
      {
       name: "Sanket Patil",
       role: "",
-      image: "/images/team images/Sanket Patil.jpg",
+      image: "/images/team images/Sanket Patil.jpeg",
       github: "https://github.com/sankettpatill",
       linkedin: "https://linkedin.com/in/sanket-patil-231b69334",
-        imageStyle: {   objectPosition: 'center -45px'},
      
     },
     {
       name: "Mohit  Patil",
       role: "",
-      image: "/images/team images/Mohit Patil.jpg",
+      image: "/images/team images/Mohit Patil.jpeg",
       github: "https://github.com/Mohitpatil712",
       linkedin: "https://www.linkedin.com/in/mohit-patil-4b191b358?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-   imageStyle: {  objectFit: 'cover',
-                  objectPosition: 'center -35px' }  
+   
     },
      {
       name: "Chaitali Borse",
       role: "",
-      image: "/images/team images/Chaitali Borse.jpg",
+      image: "/images/team images/Chaitali Borse.jpeg",
       github: "https://github.com",
       linkedin: "https://www.linkedin.com/in/chaitali-borse-4b3877330?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-      imageStyle: {  objectPosition: 'center -55px'},
-   
+    imageStyle: { objectPosition: 'center 0px' } 
     },
     {
       name: "Rutuja Deshmukh",
       role: "",
-      image: "/images/team images/Rutuja Deshmukh.jpg",
+      image: "/images/team images/Rutuja Deshmukh.jpeg",
       github: "https://github.com/RutujaDeshmukh427",
       linkedin: "https://www.linkedin.com/in/rutuja-deshmukh-59a351327",
-      imageStyle: {  objectPosition: 'center -54px'},
      
     },
     {
       name: "Pushpak Mali",
       role: "",
-      image: "/images/team images/Pushpak Mali.jpg",
+      image: "/images/team images/Pushpak Mali.jpeg",
       github: "https://github.com/pushpakmali-21",
       linkedin: "https://www.linkedin.com/in/pushpak-mali-b51b11336",
-          imageStyle: {  objectPosition: 'center -48px'},
        
     },
      {
       name: "Om Patil",
       role: "",
-      image: "/images/team images/Om Patil.jpg",
+      image: "/images/team images/Om Patil.jpeg",
       github: "https://github.com/omdipakpatil7",
       linkedin: "https://www.linkedin.com/in/om-pawar=05428330",
-      imageStyle: {   objectPosition: 'center -48px'},
       
     },
     {
       name: "Yashpal Girase",
       role: "",
-      image: "/images/team images/Yashpal Girase.jpg",
+      image: "/images/team images/Yashpal Girase.jpeg",
       github: "https://github.com/yashpalgirase122",
       linkedin: "https://www.linkedin.com/in/yashpal-girase-639b4332b",
-        imageStyle: { objectPosition: 'center -53px'},
        
     },
   ];
